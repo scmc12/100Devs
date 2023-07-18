82a79328b6e6e76cd6ac79a65afe5501f9d12cc7
mcgh12 <mcguire.holt@yahoo.com>
2023-07-18 16:56:53 -0600

    refactor

--- Class 20/reviewwinter/main.js
+++ Class 20/reviewwinter/main.js
@@ -1,12 +1,14 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
 
-function multiplyArray(arr) {
-  let product = 1;
-  // arr.forEach((num) => (product *= num));
-  for (let i = 0; i < arr.length; i++) {
-    product *= arr[i];
-  }
-  alert(product);
+function onlyEvens(nums) {
+  let evens = [];
+
+  nums.forEach((n) => {
+    if (n % 2 === 0) {
+      evens.push(n);
+    }
+  });
+  return evens;
 }
 
-multiplyArray([10, 2, 3]);
+console.log(onlyEvens([1, 2, 3, 4, 5, 6]));
