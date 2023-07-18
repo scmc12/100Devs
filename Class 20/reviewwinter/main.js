0c8f492cf660ce56c450fe5efd2f381d630ddd99
mcgh12 <mcguire.holt@yahoo.com>
2023-07-18 02:07:45 -0600

    fixed function

--- Class 20/reviewwinter/main.js
+++ Class 20/reviewwinter/main.js
@@ -1,1 +1,12 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function multiplyArray(arr) {
+  let product = 1;
+  // arr.forEach((num) => (product *= num));
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  alert(product);
+}
+
+multiplyArray([10, 2, 3]);
