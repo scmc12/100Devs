1cb400109188032083c7f08a43ff85036e6a2787
mcgh12 <mcguire.holt@yahoo.com>
2023-07-18 21:20:57 -0600

    refactor

--- Class 20/arraysworldwide/main.js
+++ Class 20/arraysworldwide/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+
+function allNumbers(nums) {
+  let numbers = [];
+
+  nums.forEach((nums) => {
+    if (nums <= Array.prototype.every(allNumbers)) {
+      return numbers;
+    }
+  });
+}
+
+console.log(allNumbers([1, 2, 3, 4, 5, 6]));
