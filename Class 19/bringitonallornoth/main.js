deb5fd9261c25525bf8ddae7c834851cb8d6ece4
mcgh12 <mcguire.holt@yahoo.com>
2023-07-19 21:03:31 -0600

    refactor

--- Class 19/bringitonallornoth/main.js
+++ Class 19/bringitonallornoth/main.js
@@ -36,9 +36,9 @@ const alertOrLog = (bool, str) => (bool ? alert(str) : console.log(str));
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
 
 function fizzBuzz(num) {
-  for (let i = 1; i <= num; i++) {
+  for (let i = 0; i <= num; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log("fizzBuzz");
+      console.log("fizzbuzz");
     } else if (i % 3 === 0) {
       console.log("fizz");
     } else if (i % 5 === 0) {
@@ -48,5 +48,4 @@ function fizzBuzz(num) {
     }
   }
 }
-
 fizzBuzz(30);
