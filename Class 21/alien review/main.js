9a36381bc7bd95d33abc80ffff74a5b2a721fe61
mcgh12 <mcguire.holt@yahoo.com>
2023-07-22 23:57:16 -0600

    solved simple problems

--- Class 21/alien review/main.js
+++ Class 21/alien review/main.js
@@ -2,8 +2,25 @@
 
 //Create and array of tv shows. Loop through and print each show to the console
 
+let tvShows = ["Sopranos", "Breaking Bad", "The Wire"];
+
+tvShows.forEach((show) => console.log(show));
+
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
 
+let myNumbers = [1, 2, 3, 4, 5, 6];
+
+let onlyEvens = (arr) => arr.filter((n) => n % 2 === 0);
+
+console.log(onlyEvens(myNumbers));
+
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+
+function sumOfSecondLowAndHigh(arr) {
+  let sorted = arr.sort((a, b) => a - b);
+  alert(sorted[1] + sorted[arr.length - 2]);
+}
+
+sumOfSecondLowAndHigh([2, 3, 1, 5, 4]);
