e3acbe6484decfef7dfe993236ab6976a0713a91
mcgh12 <mcguire.holt@yahoo.com>
2023-07-31 15:02:24 -0500

    created object

--- Class 21/objectszeo/main.js
+++ Class 21/objectszeo/main.js
@@ -1,1 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {};
+
+stopwatch.brand = "Nike";
+stopwatch.color = "black";
+stopwatch.shape = "round";
+stopwatch.size = "medium";
+
+stopwatch.start = () => {
+  console.log("starting");
+};
+
+stopwatch.stop = () => {
+  console.log("stopping");
+};
+
+stopwatch.sayBrand = () => {
+  console.log(stopwatch.brand);
+};
